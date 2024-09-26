e43a6a8f462d79a11b081130a877cf369be03e9a
mahesararslan <mahesararslan1998@gmail.com>
2024-09-26 21:37:38 +0500

    Created Basic Backend

--- backend/src/Games.ts
+++ backend/src/Games.ts
@@ -1,17 +1,91 @@
 import { WebSocket } from "ws";
+import { Chess } from "chess.js";
+import { GAME_OVER, INIT_GAME } from "./messages";
 
 export class Game {
     public player1: WebSocket;
     public player2: WebSocket;
-    private board: string;
+    private board: Chess;
     private moves: string[];
     private startTime: Date;
 
     constructor(player1: WebSocket, player2: WebSocket, ) {
         this.player1 = player1;
         this.player2 = player2;
-        this.board = "";
+        this.board = new Chess();
         this.moves = [];
         this.startTime = new Date();
+        // let both players know that the game has started
+        this.player1.send(JSON.stringify({
+            type: INIT_GAME,
+            payload: {
+                color: "white"
+            }
+        }))
+        this.player2.send(JSON.stringify({
+            type: INIT_GAME,
+            payload: {
+                color: "black"
+            }
+        }))
+    }
+
+    makeMove(socket: WebSocket, move: {
+        from: string;
+        to: string;
+    }) {
+        // validate the type of move using zod
+
+        // check if it is the player's turn
+        if (this.board.turn() === 'w' && socket === this.player2) {
+            return;
+        }
+
+        if (this.board.turn() === 'b' && socket === this.player1) {
+            return;
+        }
+        
+        try {
+
+            this.board.move(move)
+        } catch (error) {
+            // send an error message to the player
+            return;
+        }
+
+        if (this.board.isGameOver()) { // Stringify it because you can only send strings over the websockets
+            this.player1.emit (JSON.stringify({
+                type: GAME_OVER,
+                payload: {
+                    winner: this.board.turn() === 'w' ? 'black' : 'white'
+                }
+            }))
+            this.player2.emit (JSON.stringify({
+                type: GAME_OVER,
+                payload: {
+                    winner: this.board.turn() === 'w' ? 'black' : 'white'
+                }
+            }))
+        }
+
+        if (this.board.moves.length % 2 === 0) {
+            this.player2.emit(JSON.stringify({
+                type: "move",
+                payload: move
+            }))
+        } else {
+            this.player1.emit(JSON.stringify({
+                type: "move",
+                payload: move
+            }))
+        }
+
+
+        // update the board
+        // push the move
+
+        // check if the game is over
+        // send the updated board to both players
+        return;
     }
 }
