8984ef1ef19fc887a7e1be0ad3cd070009639e12
mahesararslan <mahesararslan1998@gmail.com>
2025-03-23 03:44:05 +0500

    Fixes google auth redirect 3

--- frontend/src/screens/Signin.tsx
+++ frontend/src/screens/Signin.tsx
@@ -54,8 +54,7 @@ function SignInPage() {
 
   const handleGoogleSignIn = (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
     event.preventDefault();
-    const BASE_URL = import.meta.env.VITE_NODE_BACKEND_URL.replace(/\/+$/, '');
-    window.location.replace(`${BASE_URL}auth/google`);
+    window.location.href = `${import.meta.env.VITE_NODE_BACKEND_URL}/auth/google`;
 
   }
 
